d12c72c4b875cf2a4a77a54e81b72da8b1da96ae
Gianelle Tianco <gianelle.tianco@gmail.com>
2022-02-07 02:20:40 -0500

    Exact Path

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/projects">
+        <Route exact path="/projects">
           <Projects />
         </Route>
       </Switch>
